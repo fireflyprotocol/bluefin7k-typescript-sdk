cde782150ae41ac0f720343e0ee316439f2bc2d7
Urwah Sajjad <36397831+usajjad123@users.noreply.github.com>
2025-11-19 12:41:47 +0300

    [BET-2890]: added Bluefin-Aggregator-API-Key support within sdk (#23)

--- src/config/fetchClient.ts
+++ src/config/fetchClient.ts
@@ -7,12 +7,17 @@ export async function fetchClient(
   const headers = new Headers(init?.headers);
   const apiKey = (Config.getApiKey() || "").trim();
   const bluefinXApiKey = (Config.getBluefinXApiKey() || "").trim();
+  const bluefinAggregatorApiKey = (Config.getBluefinAggregatorApiKey() || "").trim();
   if (apiKey) {
     headers.set("apiKey", apiKey);
   }
 
   if (bluefinXApiKey) {
     headers.set("Bluefin-X-API-Key", bluefinXApiKey);
+  }
+
+  if (bluefinAggregatorApiKey) {
+    headers.set("Bluefin-Aggregator-API-Key", bluefinAggregatorApiKey);
   }
 
   const modifiedInit: RequestInit = {
